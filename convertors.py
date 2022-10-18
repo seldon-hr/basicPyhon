a84a8b1fdc380e0c7b225405d20995f521641e01
Daniel Romero <daanmaro99june@gmail.com>
2022-10-18 12:54:06 -0500

    Using basic python

---
+++ convertors.py
@@ -0,0 +1,11 @@
+# int(var) variable to int
+# float(var) varialbe to float
+# str(var) variable to txt
+# bool(var) variable to boolean
+# abs(var) variable to valor absoluto
+
+#Ways to converts type of data in pyhton
+
+valor = input('Ingresa el valor boolean: ')
+valor = bool(valor)
+print(valor)
