ce3cd5685936bc5a281e603b83209c9009f07455
Daniel Romero <daanmaro99june@gmail.com>
2022-10-18 13:00:18 -0500

    Lógicos y de Comparación

---
+++ 12_logic_operators.py
@@ -0,0 +1,27 @@
+es_estudiante = True
+trabaja = False
+
+
+#Opearadores lógicos
+es_estudiante and trabaja
+#Output False
+
+es_estudiante or trabaja
+#Output False
+
+not es_estudiante
+print(es_estudiante)
+#False
+
+#Oparadores de Comparación
+num1 = 5
+num2 = 5
+
+num1 == num2
+#Output True
+
+num1 != num2
+#False
+
+num1 > num2
+#Fals
