2affd7408f89c30e3ec626792ee7dec26eff7b62
Daniel Romero <daanmaro99june@gmail.com>
2022-10-19 13:01:44 -0500

    Conditionals

---
+++ 14_condicionales.py
@@ -0,0 +1,19 @@
+# age = int(input("Insert your age: "))
+# if age > 17:
+#     print ("You're enough mature") 
+#     pass #pass is a word to indicate that later you're going to write the rest of the code.
+# else:
+#     print("You're not enough mature")
+
+num = int(input("Insert a numbe:  "))
+
+if num > 5:
+    print("It's larger than 5")
+elif num == 5:
+    print("It's equal to 5")
+else:
+    print("It's less than 5")
+
+#elif = else if other case es 'sino' 
+#Remember always use four space after the if or funciton code 
+# four spaces or a tab. the opposite python couldn't work well.
