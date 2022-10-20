570f807b64ccffeba3cac10ae374eb952eb33ddf
Daniel Romero <daanmaro99june@gmail.com>
2022-10-20 12:23:08 -0500

    Using methods by strings

---
+++ 18_strings.py
@@ -0,0 +1,26 @@
+#Métodos para interactuar con strings
+
+"""
+    name = name.upper()
+    #Transform all the string in uppercase
+
+    name = name.capitalize()
+    Transform in uppercaes the first letter
+
+    name = name.strip()
+    Deleate the space if you typed by wrong.
+
+    name = name.lower()
+    Transform all letters in lowercase
+
+    name = name.replace('o', 'a')
+    Replace each kind of 'o' letter by 'a'
+
+    name[0]
+    You acces with the index with your string
+
+    len(name)
+    Access to the length of a strings
+    
+
+"""
