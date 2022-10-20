9405165f651c139b72b748340e52dae290a38cee
Daniel Romero <daanmaro99june@gmail.com>
2022-10-20 12:28:30 -0500

    Use slice to strings

--- 18_strings.py
+++ 18_strings.py
@@ -21,6 +21,29 @@
 
     len(name)
     Access to the length of a strings
-    
+
+
+
+
+    Slices in python strings
+
+    name = 'Crescencio'
+    name[0]
+    Output -> 'C'
+
+    To slice
+
+    name[0:3]  or name [:3]
+    'Cre'
+    Python corta antes del tres, así obtengo esta parte del texto
+
+    name[3:]
+    Desde dónde va a partir.
+
+    name[1:7:2]
+    Señala el número de los indices, pero en cuantos vas a contar.
+
+    name[::-1]
+    Voy a empezar a contar a partir desde el final.
 
 """
