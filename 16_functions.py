b5be0ad8e52934bf25a1284f745d677968dd422d
Daniel Romero <daanmaro99june@gmail.com>
2022-10-20 12:03:06 -0500

    Use return in function

--- 16_functions.py
+++ 16_functions.py
@@ -9,18 +9,30 @@
 
 # print_message()
 
-def conversation(message):
-    print('hi')
-    print('how are you')
-    print(message)
-    print('bye')
+# def conversation(message):
+#     print('hi')
+#     print('how are you')
+#     print(message)
+#     print('bye')
 
-option = input('Choose an option (1, 2, 3): ')
-if option == 1:
-    conversation('Choose option 1')
-elif option == 2:
-    conversation('Choose option 2')
-elif option == 3:
-    conversation('Choose option 3')
-else: 
-    print('Choose other option')
+# option = input('Choose an option (1, 2, 3): ')
+# if option == 1:
+#     conversation('Choose option 1')
+# elif option == 2:
+#     conversation('Choose option 2')
+# elif option == 3:
+#     conversation('Choose option 3')
+# else: 
+#     print('Choose other option')
+
+
+from unittest import result
+
+
+def sum(a, b):
+    print('Sum of two numbers')
+    result = a + b;
+    return result
+
+sumatoria = sum(1, 3)
+print (sumatoria)
