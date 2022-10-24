f2a37d8f0f017373a12aa2aab21154b871bd4f35
Daniel Romero <daanmaro99june@gmail.com>
2022-10-24 13:14:35 -0500

    First use of loops

---
+++ potencia.py
@@ -0,0 +1,12 @@
+""" Print potencia de uno en uno """
+# def run():
+#     for x in range(10):
+#         print(x ** 2)
+
+
+# if __name__ == '__main__':
+#     run()
+
+
+""" Improving the python code """
+
