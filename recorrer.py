b686697e5505a3093a7462c9facabc7cccd8459e
Daniel Romero <daanmaro99june@gmail.com>
2022-10-24 14:22:11 -0500

    Use for to strings

---
+++ recorrer.py
@@ -0,0 +1,14 @@
+#Using for in strings
+def run():
+    # name = input('Type your name: ')
+    # for letra in name:
+    #     print(letra)
+
+    frase = input('Type a phrase: ')
+    for caracter in frase:
+        print(caracter.upper())
+
+
+
+if __name__ == '__main__':
+    run()
