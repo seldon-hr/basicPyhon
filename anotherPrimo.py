aacee6ee564600125a883a2c6664a657c7ead40e
Daniel Romero <daanmaro99june@gmail.com>
2022-10-25 12:56:16 -0500

    Challenge: Un número es primo o no

--- anotherPrimo.py
+++ anotherPrimo.py
@@ -1,14 +1,24 @@
-def calcular_factorial(numero):
+def calcular_factorial_menos_uno_y_uno_mas_al_final(numero):
+    numero-=1
     contador = numero-1
     while contador != 1:
         numero = numero * contador
         contador -= 1
-    return numero
+    return numero + 1
 
+def es_primo(numero):
+    multiplo = calcular_factorial_menos_uno_y_uno_mas_al_final(numero)
+    if multiplo % numero == 0:
+        return True
+    else: 
+        return False
 
 def run():
     numero = int(input('Escribe un número: '))
-    print('Factorial: ' + str(calcular_factorial(numero)))
+    if es_primo (numero):
+        print('Es primo')
+    else:
+        print('No es primo')
 
 
 
