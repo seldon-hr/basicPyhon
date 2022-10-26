bfc4f6c2dbc29879bad574c3925d4bdf27486ccc
Daniel Romero <daanmaro99june@gmail.com>
2022-10-26 12:44:14 -0500

    tuplas

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -1,7 +1,8 @@
 import random
+#importa una librería para poder hacer operaciones con números aleatorios.
 
 def run():
-    numero_aleatorio = random.randint(1, 100)
+    numero_aleatorio = random.randint(1, 100) #Aquí la usamos, y para esto, indicamos que nos brinde la opción de un rango de números, y específicamos el rango de números sobre nuestro aleatorio
     numero_elegido = int(input('Elige un número del 1 al 100:'))
     while numero_elegido != numero_aleatorio:
         if numero_elegido < numero_aleatorio:
